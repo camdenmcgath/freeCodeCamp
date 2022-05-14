c7e42a133f7ac20fdbd053382bfffc009dd25d45
camocodes <clm2930@comcast.net>
2022-05-14 16:37:18 -0700

    commit

--- ScientificComputingWithPython/boilerplate-budget-app/budget.py
+++ ScientificComputingWithPython/boilerplate-budget-app/budget.py
@@ -1,4 +1,5 @@
-# TODO: Category.view not part of assignment
+# TODO: build create_spend_chart func, remove test func calls
+# TODO: make sure amounts in budget object output are max 7 characters
 class Category:
     def __init__(self, name):
         self.ledger = []
@@ -47,22 +48,44 @@ class Category:
     def __str__(self):
         returnstr = f"{self.name.center(30, '*')}\n"
         for i in self.ledger:
-            returnstr += i["description"].ljust(23)
-            returnstr += str(i["amount"]).rjust(7)
+            amount = i["amount"]
+            description = i["description"]
+            returnstr += description[:23].ljust(23)
+            returnstr += f"{amount:.2f}".rjust(7)
             returnstr += "\n"
+        returnstr += f"Total: {self.get_balance():.2f}"
         return returnstr
 
 
-def create_spend_chart(categories):
-    pass
-
-
 # tests for Category class methods
-gas = Category("gas")
-food = Category("food")
-food.deposit(10)
-gas.deposit(40)
-gas.transfer(food, 6.5)
-food.view()
-gas.view()
+clothing = Category("Clothing")
+food = Category("Food")
+food.deposit(1000, "initial deposit")
+food.withdrawal(10.15, "grocieries")
+food.withdrawal(15.89, "restaurant and more food")
+food.transfer(clothing, 50)
 print(food)
+
+
+def create_spend_chart(categories):
+    withdrawals = []
+    total_spent = 0
+    for cat in categories:
+        withdrawal_total = 0
+        for trans in cat.ledger():
+            if trans["amount"] < 0:
+                withdrawal_total += trans["amount"]
+        withdrawals.append(withdrawal_total)
+        total_spent += withdrawal_total
+
+    percentages = [((x / total_spent) * 100) // 10 for x in withdrawals]
+    # TODO build bar graph format
+    # TODO build an array of size ll and fill with 'o's and spaces
+    # use loop to append bar graph data to bar_chart
+    # use range(0,len(categories)) to find column number
+    # similar method for category name list, characters from end to start (spaces included)
+    # also use loop to print out until longest name has been printed
+
+    bar_chart = "Percentage spent by category\n"
+
+    return bar_chart
